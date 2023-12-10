db445cb9de682040846bc966ef7004c9ba47c18f
Stephen Pflug <stephen@pflug.us>
2023-12-10 13:06:33 -0600

    first commit. updated gitignore

---
+++ src/main/java/com/pfirewire/cookbackend/CookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pfirewire.cookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookBackendApplication.class, args);
+    }
+
+}
